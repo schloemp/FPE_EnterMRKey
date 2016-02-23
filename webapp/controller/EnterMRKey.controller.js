813149589cff6bce34b905b0ba50660f89f60716
AndrÃ© Schloemp <andre.schloemp@prologa.de>
2016-02-23 16:08:53 +0000

    qwerty

--- webapp/controller/EnterMRKey.controller.js
+++ webapp/controller/EnterMRKey.controller.js
@@ -1,13 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
-
 	return Controller.extend("EnterMRKey.controller.EnterMRKey", {
+		selectedKey: undefined,
+		PEStepContainer: undefined,
+		onInit: function() {
+			var lPE = sap.ushell.Container.getService("ProcessEngine");
+			this.PEStepContainer = lPE.getCurrentStepContainer();
+			var lModel = new JSONModel({
+				CustomerName: this.PEStepContainer.CustomerName
+			});
+			this.getView().setModel(lModel, "data");
+		},
 		onSubmit: function() {
-		    var lPE = sap.ushell.Container.getService("ProcessEngine");
-		    lPE.executeNext();
+			if (this.selectedKey) {
+				var lPE = sap.ushell.Container.getService("ProcessEngine");
+				lPE.executeNext();
+			}
+		},
+		/**
+		 *@memberOf EnterMRKey.controller.EnterMRKey
+		 */
+		onSelectionChanged: function(aEvent) {
+			if (aEvent.getParameter("selected")) {
+				this.selectedKey = aEvent.getParameter("listItem").getTitle();
+				this.PEStepContainer.MROrderKey = this.selectedKey;
+			} else {
+				this.PEStepContainer.MROrderKey = undefined;
+			}
 		}
 	});
-
 });
